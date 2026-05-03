c12a488cfe0e901dc6ce51dd75f55e0a40ee17a5
Judith OG <judith.og02@gmail.com>
2026-05-03 20:00:44 +0200

    update

--- src/data_preparation/merge_and_quality.py
+++ src/data_preparation/merge_and_quality.py
@@ -1,6 +1,7 @@
 from chembl_webresource_client.new_client import new_client
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 df_acts_assays = pd.read_csv("data/partials/db_final.csv")
 df_conf_assays = pd.read_csv("data/partials/conf_assays_types.csv")
@@ -33,6 +34,21 @@ print(df_acts['assay_type'].value_counts(dropna=False)) # només B i F
 df_complete = df_acts.groupby(['molecule_chembl_id', 'target_chembl_id'], 
                                      as_index = False)['pchembl_value'].median()
 print(len(df_complete)) # 245614 despres de la mediana
+
+# Distribució de les dades:
+print("Mitjana pChEMBL:", round(df_complete['pchembl_value'].mean(), 4))
+print("STD pChEMBL:", round(df_complete['pchembl_value'].std(), 4))
+
+# Histograma pChEMBL:
+plt.figure(figsize = (8, 5))
+plt.hist(df_complete['pchembl_value'], bins = 50, color = 'salmon', edgecolor = 'whitesmoke')
+plt.axvline(x = 6.5, color = 'red', linestyle = '--', linewidth = 1.5, label = 'Llindar activitat (pChEMBL = 6.5)')
+plt.xlabel('pChEMBL')
+plt.ylabel('Nombre de registres')
+plt.legend()
+plt.tight_layout()
+plt.savefig("figures/pchembl_distribution.png", dpi = 300)
+plt.show()
 
 # Classificació actius/inactius segons valor pChEMBL:
 df_complete['activity_type'] = np.where(
